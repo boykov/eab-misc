b8abd8cf5a96f54c261e1b15d259c51b9dbdd323
Evgeny Boykov <artscan@list.ru>
2026-09-17 17:18:36 +1000

    debug pandoc parameters and output string

--- ob-markdown.el
+++ ob-markdown.el
@@ -5,25 +5,17 @@
   "Execute a block of markdown code with org-babel.
 This function is called by `org-babel-execute-src-block'."
   (let* ((result-params (split-string (or (cdr (assoc :results params)) "")))
-	 (out-file ((lambda (el)
-		      (or el
-			  (error
-			   "markdown code block requires :file header argument")))
-		    (cdr (assoc :file params))))
-	 (in-file (org-babel-temp-file "markdown-"))
-	 (cmdline (cdr (assoc :cmdline params)))
-	 (to (cdr (assoc :to params)))
-	 (template (cdr (assoc :template params)))	 
-	 (cmd (concat "pandoc"
-		      " -o " (org-babel-process-file-name out-file)
-		      " -i " (org-babel-process-file-name in-file)
-		      " -f markdown "
-		      " -t " to
-		      " --template " template
-		      " " cmdline)))
+         (in-file (org-babel-temp-file "markdown-"))
+         (cmdline (cdr (assoc :cmdline params)))
+         (to (cdr (assoc :to params)))
+         (template (cdr (assoc :template params)))
+         (cmd (concat "pandoc"
+                      " -i " (org-babel-process-file-name in-file)
+                      " -f markdown "
+                      " -t org " to
+                      " " cmdline)))
     (with-temp-file in-file (insert body))
-    (message cmd) (shell-command cmd)
-    nil)) ;; signal that output has already been written to file
+    (message cmd) (shell-command-to-string cmd))) ;; signal that output has already been written to file
 
 (defun org-babel-prep-session:markdown (session params)
   "Return an error because markdown does not support sessions."
